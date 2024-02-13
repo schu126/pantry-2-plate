a55b117639c5794ec56162d144e1ddf5e3635a3a
Samuel Chu <samueljchu0126@gmail.com>
2024-02-13 00:28:33 -0500

    comments on explanation of the code

--- index.js
+++ index.js
@@ -40,12 +40,15 @@ function searchRecipe (protein, vegetable, carb) {
 
     const filteredRecipes = recipes.filter((recipe) => {
         return (
+            // compares the recipe's property of protein, veg, and carb and user's input of protein, veg, and carb in lowercase so that there are no case sensitive errors.
+            // checks if the user's input is present in our database using the includes function
+            // if all the criteria is met, function will return true, and provide matching recipe. Might have to tinker with the boolean statement however.
             recipe.protein.toLowerCase().includes(protein.toLowerCase()) &&
-            recipe.vegetable.to lowerCase().includes(vegetable.toLowerCase())&&
+            recipe.vegetable.toLowerCase().includes(vegetable.toLowerCase())&&
             recipe.carb.toLowerCase().includes(carb.toLowerCase())
         );
     });
-
+    //if the array length is greater than 0, the first found/filtered recipe will be shown.
     if (filteredReceipes.length > 0) {
         const foundRecipe = filteredRecipes[0];
         document.getElementById('recipe').querySelector('h2').textContent = foundRecipe.dishName;
