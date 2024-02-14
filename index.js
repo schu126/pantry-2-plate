791029561c0a326a1042a4e21296fc35391a7245
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-02-13 22:25:42 -0500

    changed many ids to class in html and css, created  a forEach loop through pantry sections to get buttons   working and creating lists

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-const recipeURL = "some url here"
+// const recipeURL = "some url here"
 
-fetch (recipeURL)
-.then ((response) => response.json())
-.then((recipes) => {
-    recipes.forEach(recipe) => {
-        renderRecipe(recipe)
-    }
-});
+// fetch (recipeURL)
+// .then ((response) => response.json())
+// .then((recipes) => {
+//     recipes.forEach(recipe) => {
+//         renderRecipe(recipe)
+//     }
+// });
 
 // need to include a function about filtering (gotta study up on)
 
-const renderRecipe = (recipe) => {
-    let div = 
-}
+// const renderRecipe = (recipe) => {
+//     let div = 
+// }
 
-const handleClick = () => {
+// const handleClick = () => {
 
-};
+// };
 
-const addSubmitListener = () => {
+// const addSubmitListener = () => {
 
-}
+// }
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ingredientSections = document.querySelectorAll(".ingredientSection")
+    ingredientSections.forEach((ingredientSection) => {
+
+  //  const ingredientSection = document.querySelectorAll(".ingredientSection") [0]
+    console.log(ingredientSection)
+    const input = ingredientSection.querySelector(".ingredient-pantry-input")
+  
+    const button = ingredientSection.querySelector(".submit")
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(input.value)
+      
+      const list = ingredientSection.querySelector(".ingredientList")
+      const listItem = document.createElement("li")
+      const span = document.createElement("span")
+      span.textContent = input.value
+      
+      listItem.append(span)
+      list.append(listItem)
+      
+    })
+
+    })
+
+});
